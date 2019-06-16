cc08a68d2d34b55e1055b64186936f0c7159c816
Diego <diego.humberto@compila.com.br>
2019-06-16 20:54:16 -0300

    Updates

---
+++ precache-manifest.5686c36fcdbea0b0378927dabd164bae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d23ce34938f610e61e798fc0b1ee6c",
+    "url": "/search_products_ml/index.html"
+  },
+  {
+    "revision": "d1036c895424ee4bea53",
+    "url": "/search_products_ml/static/css/main.120c7c76.chunk.css"
+  },
+  {
+    "revision": "1b1756be9ad4c6d794b9",
+    "url": "/search_products_ml/static/js/2.dc456c6b.chunk.js"
+  },
+  {
+    "revision": "d1036c895424ee4bea53",
+    "url": "/search_products_ml/static/js/main.3b9570cf.chunk.js"
+  },
+  {
+    "revision": "f9ca5eb86fb3a9f73710",
+    "url": "/search_products_ml/static/js/runtime~main.131e3df7.js"
+  }
+]);
