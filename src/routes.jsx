0699ffc0ed545e0ea678d1ed4f8ac4644bfd91fe
Diego <diego.humberto@compila.com.br>
2019-06-16 21:18:05 -0300

    gh-pages com erro

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
 
 import Search from './pages/Search'
 import Product from './pages/Product'
 
 const Routes = () => {
     return (
-        <Switch>
-            <Route exact path='/' component={Search} />
-            <Route exact path='/product/:id' component={Product} />
-            <Route component={() => (
-                <div> Page not found</div>
-            )}
-            />
-        </Switch>
+        <BrowserRouter basename="/">
+            <Switch>
+                <Route exact path='/' component={Search} />
+                <Route exact path='/product/:id' component={Product} />
+                <Route component={() => (
+
+                    <div> Page not found</div>
+                )}
+                />
+            </Switch>
+
+        </BrowserRouter>
     )
 }
 
